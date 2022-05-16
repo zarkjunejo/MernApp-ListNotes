41fc69d1ff17c18621817dce623a0a5835b9fbd8
zarkjunejo <zarkjunejo@gmail.com>
2022-05-16 17:58:38 +0500

    modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 var cors = require('cors')
 // const mongoose = require('mongoose');
-
+const path = require('path');
 
 const connectToMongo = require("./db");
 
@@ -20,10 +20,13 @@ const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV == "production") {
 
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static('build'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    });
 }
 
-// app.get('/', function (req, res) {
+// app.get('/', function (req, res) { 
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
 
